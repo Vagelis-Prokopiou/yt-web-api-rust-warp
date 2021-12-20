1438771b7ad59d7b8f9092ea777d20951c779937
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-20 20:53:46 +0200

    Removing import

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 use tokio::runtime::Builder;
 use warp::Filter;
 
-use api_warp::get_users;
-
 fn main() {
-    // build runtime
     let runtime = Builder::new_multi_thread()
         .enable_all()
         .worker_threads(8)
@@ -17,7 +14,7 @@ fn main() {
             .and(warp::path("v1"))
             .and(warp::path("users"))
             .and(warp::path::end())
-            .map(|| warp::reply::json(&get_users()));
+            .map(|| warp::reply::json(&api_warp::get_users()));
 
         warp::serve(users_route).run(([127, 0, 0, 1], 8082)).await;
     });
