c7c1e55f18241b8e213c39a38907d0fb5d3254f5
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-02-06 13:00:27 +0200

    Best setup

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,8 @@ use api_warp::then_get_users;
 fn main() {
     let runtime = Builder::new_multi_thread()
         .enable_all()
-        .worker_threads(num_cpus::get_physical())
+        //.worker_threads(num_cpus::get_physical())
+        .worker_threads(num_cpus::get()) // Best result
         .build()
         .unwrap();
 
@@ -15,8 +16,8 @@ fn main() {
     runtime.block_on(async {
         let users_route = warp::path("users")
             .and(warp::path::end())
-            //.and_then(and_then_get_users);
-            .then(then_get_users);
+            // .and_then(and_then_get_users);
+            .then(then_get_users); // Best result
 
         println!("\nServer running at: http://127.0.0.1:8082/users");
         warp::serve(users_route).run(([127, 0, 0, 1], 8082)).await;
