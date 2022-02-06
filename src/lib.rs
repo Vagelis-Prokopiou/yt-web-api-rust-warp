45e6e5587cd82411a188e0e8f05f472a370f480d
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2022-02-06 12:51:44 +0200

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,48 @@
+use std::convert::Infallible;
+
+use warp::reply::Json;
+use warp::{Rejection, Reply};
+
 use crate::models::User;
 
 pub mod models;
 
-pub fn get_users() -> Vec<User> {
-    let mut users = Vec::with_capacity(1000);
-    for index in 1..1001_u16 {
-        users.push(User {
-            Id: index,
-            Age: 25,
-            First_Name: format!("First_Name{}", index),
-            Last_Name: format!("Last_Name{}", index),
-            Framework: "Rust (Warp)".to_owned(),
-        })
-    }
-    users
+pub async fn and_then_get_users() -> Result<impl warp::Reply, Infallible> {
+    let users = tokio::task::spawn_blocking(|| {
+        let mut users = Vec::with_capacity(1000);
+        for index in 1..1001_u16 {
+            users.push(User {
+                Id: index,
+                Age: 25,
+                First_Name: format!("First_Name{}", index),
+                Last_Name: format!("Last_Name{}", index),
+                Framework: "Rust (Warp)".to_owned(),
+            })
+        }
+        users
+    })
+    .await
+    .unwrap();
+
+    Ok(warp::reply::json(&users))
+}
+
+pub async fn then_get_users() -> impl warp::Reply {
+    let users = tokio::task::spawn_blocking(|| {
+        let mut users = Vec::with_capacity(1000);
+        for index in 1..1001_u16 {
+            users.push(User {
+                Id: index,
+                Age: 25,
+                First_Name: format!("First_Name{}", index),
+                Last_Name: format!("Last_Name{}", index),
+                Framework: "Rust (Warp)".to_owned(),
+            })
+        }
+        users
+    })
+    .await
+    .unwrap();
+
+    warp::reply::json(&users)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use tokio::runtime::Builder;
 use warp::Filter;
 
+use api_warp::and_then_get_users;
+use api_warp::then_get_users;
+
 fn main() {
     let runtime = Builder::new_multi_thread()
         .enable_all()
@@ -12,7 +15,8 @@ fn main() {
     runtime.block_on(async {
         let users_route = warp::path("users")
             .and(warp::path::end())
-            .map(|| warp::reply::json(&api_warp::get_users()));
+            //.and_then(and_then_get_users);
+            .then(then_get_users);
 
         println!("\nServer running at: http://127.0.0.1:8082/users");
         warp::serve(users_route).run(([127, 0, 0, 1], 8082)).await;
